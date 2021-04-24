1ead9ccd25f845b2e8736292c4033079a8f9b7d2
Mcvean Soans <mc.soans02@gmail.com>
2021-04-24 19:07:21 +0530

    create: card components

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import NoteCard from '../components/NoteCard';
 
 const Notes = () => {
     const [notes, setNotes] = useState([]);
@@ -7,14 +10,26 @@ const Notes = () => {
         fetch('http://localhost:8000/notes')
             .then(res => res.json())
             .then(data => setNotes(data))
-    })
+    }, []);
+
+    const handleDelete = async (id) => {
+        await fetch('http://localhost:8000/notes/' + id, {
+            method: 'DELETE'
+        })
+        const newNotes = notes.filter(note => note.id != id)
+        setNotes(newNotes)
+    }
 
     return (
-        <div>
-            {notes.map(note => (
-                <p key={note.id}>{note.title}</p>
-            ))}
-        </div>
+        <Container>
+            <Grid container spacing={3}>
+                {notes.map(note => (
+                    <Grid item xs={12} md={6} lg={4} key={note.id}>
+                        <NoteCard note={note} handleDelete={handleDelete} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
     );
 }
  
