567e25dea3d7cf52063538902383d955dc81f95a
Mcvean Soans <mc.soans02@gmail.com>
2021-04-25 19:03:03 +0530

    create: masonry layout for notes

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -4,12 +4,39 @@ import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
+import { makeStyles } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import { yellow, green, pink, blue } from '@material-ui/core/colors';
+
+const useStyles = makeStyles({
+    avatar: {
+        backgroundColor: (note) => {
+            if (note.category === 'work') {
+                return yellow[700]
+            }
+            if (note.category === 'money') {
+                return green[500]
+            }
+            if (note.category === 'todos') {
+                return pink[500]
+            }
+            return blue[500]
+        },
+    }
+});
 
 const NoteCard = ({ note, handleDelete }) => {
+    const classes = useStyles(note);
+
     return (
         <div>
             <Card elevation={1}>
                 <CardHeader
+                    avatar={
+                        <Avatar className={classes.avatar}>
+                            {note.category[0].toUpperCase()}
+                        </Avatar>
+                    }
                     action={
                         <IconButton onClick={() => handleDelete(note.id)}>
                             <DeleteOutlined />
